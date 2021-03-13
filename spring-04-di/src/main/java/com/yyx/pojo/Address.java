a719682ae3cb7fd34baa84bf5a9909a05b81a582
yongxin <965224591@qq.com>
2021-03-13 21:57:56 +0800

    Spring学习

---
+++ spring-04-di/src/main/java/com/yyx/pojo/Address.java
@@ -0,0 +1,21 @@
+package com.yyx.pojo;
+
+public class Address {
+
+        private String address;
+
+        public String getAddress() {
+            return address;
+        }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "address='" + address + '\'' +
+                '}';
+    }
+
+    public void setAddress(String address) {
+            this.address = address;
+        }
+    }
